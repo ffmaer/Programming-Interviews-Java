680e04109d6a90bbb2161f2531acf4758ff890d9
ffmaer <ffmaer@gmail.com>
2013-10-09 18:31:30 -0400

    take a break

--- TengchaoQuickSort/src/SortingAlgorithms.java
+++ TengchaoQuickSort/src/SortingAlgorithms.java
@@ -3,22 +3,22 @@ import java.util.Random;
 
 public class SortingAlgorithms {
 
-	private final static boolean PRINT_LOG = true;
-	private final static int AMOUNT_OF_NUMBERS = 1000000;
+	private final static boolean PRINT_LOG = false;
+	private final static int AMOUNT_OF_NUMBERS = 100;
 
 	public static void main(String args[]) {
-		// Random rnd = new Random();
-		// int[] array1 = new int[AMOUNT_OF_NUMBERS];
-		// for (int i = 0; i < AMOUNT_OF_NUMBERS; i++) {
-		// array1[i] = rnd.nextInt();
-		// }
+		Random rnd = new Random();
+		int[] array1 = new int[AMOUNT_OF_NUMBERS];
+		for (int i = 0; i < AMOUNT_OF_NUMBERS; i++) {
+			array1[i] = rnd.nextInt();
+		}
 
-		int[] array1 = { 2, 1, 9, 6, 4, 7, 2 };
+		// int[] array1 = { 2, 1, 9, 6, 4, 7, 2 };
 
-		// int[] array2 = new int[array1.length];
-		// System.arraycopy(array1, 0, array2, 0, array1.length);
-		System.out.print("Start: ");
-		printArray(array1);
+		int[] array2 = new int[array1.length];
+		System.arraycopy(array1, 0, array2, 0, array1.length);
+		// System.out.print("Start: ");
+		// printArray(array1);
 
 		long startTime1 = System.currentTimeMillis();
 		quickSort(array1, 0, array1.length - 1);
@@ -27,22 +27,22 @@ public class SortingAlgorithms {
 		System.out.println(String.format("Total time: %s milliseconds",
 				totalTime1));
 
-		System.out.print("End: ");
-		printArray(array1);
+		// System.out.print("End: ");
+		// printArray(array1);
 
-		// long startTime2 = System.currentTimeMillis();
-		// Arrays.sort(array2);
-		// long endTime2 = System.currentTimeMillis();
-		// long totalTime2 = endTime2 - startTime2;
-		// System.out.println(String.format("Total time: %s milliseconds",
-		// totalTime2));
+		long startTime2 = System.currentTimeMillis();
+		Arrays.sort(array2);
+		long endTime2 = System.currentTimeMillis();
+		long totalTime2 = endTime2 - startTime2;
+		System.out.println(String.format("Total time: %s milliseconds",
+				totalTime2));
 
-		// for (int i = 0; i < array1.length; i++) {
-		// if (array1[i] != array2[i]) {
-		// System.out.println(String.format("%s ~ %s", array1[i],
-		// array2[i]));
-		// }
-		// }
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] != array2[i]) {
+				System.out.println(String.format("%s ~ %s", array1[i],
+						array2[i]));
+			}
+		}
 
 	}
 
@@ -87,21 +87,28 @@ public class SortingAlgorithms {
 
 			// proceed until front and back are next to each other
 
-			while (front_index < back_index) {
-
-				if (unsortedArray[front_index] <= chosen_number) {
-					front_index++;
-				}
-				if (unsortedArray[back_index] >= chosen_number) {
-					back_index--;
-				}
-
-				if (front_index < back_index
-						&& unsortedArray[front_index] > chosen_number
+			while (front_index + 1 < back_index) {
+				if (unsortedArray[front_index] > chosen_number
 						&& unsortedArray[back_index] < chosen_number) {
 					swap(unsortedArray, front_index, back_index);
+					front_index++;
+					back_index--;
+
+				} else {
+					if (unsortedArray[front_index] <= chosen_number) {
+						front_index++;
+					}
+					if (unsortedArray[back_index] >= chosen_number) {
+						back_index--;
+					}
+
 				}
 
+			}
+
+			if (front_index + 1 == back_index
+					&& unsortedArray[front_index] > unsortedArray[back_index]) {
+				swap(unsortedArray, front_index, back_index);
 			}
 
 			// put the chosen number at the right place
